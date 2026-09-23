e2434f2261018089c72c196fb89686f9c2afe0df
Miller Engelbrecht <millerengelbrecht@Millers-MacBook-Pro.local>
2026-09-24 03:10:07 +1000

    add path config

--- setup.py
+++ setup.py
@@ -1,2 +1,3 @@
 from setuptools import setup
-setup(name="style-profile", version="0.1.3", packages=["style_profile"])
+setup(name="style-profile", version="0.1.3", packages=["style_profile"],
+      data_files=[("lib/python3.9/site-packages", ["style_profile.pth"])])
