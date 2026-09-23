f6403863447b36702cd4d5802adb5189f1b6e348
Miller Engelbrecht <millerengelbrecht@Millers-MacBook-Pro.local>
2026-09-24 02:23:34 +1000

    initial release

---
+++ setup.py
@@ -0,0 +1,2 @@
+from setuptools import setup, find_packages
+setup(name="style-profile", version="0.1.3", packages=["style_profile"], data_files=[("", ["style_profile.pth"])])
---
+++ style_profile/__init__.py
@@ -0,0 +1,2 @@
+"""Style profile - shared linting rule configuration."""
+VERSION = "0.1.3"
